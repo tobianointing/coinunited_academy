e5fceeee1f2f1d93b1eb53d73c751c6be3cea6dd
DevDarphiz <you@example.com>
2022-10-02 00:38:41 +0100

    first commit

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -12,7 +12,7 @@ const defaultOptions: DefaultOptions = {
   }
   
 export const client = new ApolloClient({
-    uri: `${process.env.NEXT_PUBLIC_WP_API_URL}/index.php?graphql`,
+    uri: 'https://darp.coinunited.io/index.php?graphql',
     cache: new InMemoryCache(),
     defaultOptions: defaultOptions
 })
